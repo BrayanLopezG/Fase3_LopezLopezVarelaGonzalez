5eb4001337b788d58536e384656a231ac9d74725
brayanlml <lopez.vicio.2b@gmail.com>
2021-01-28 16:11:50 -0300

    Requerimientos Funcionales de Admistrador y Usuarios

---
+++ trabajo3/pelicula/urls.py
@@ -0,0 +1,20 @@
+from django.urls import path
+from . import views
+
+urlpatterns=[ 
+    path('', views.index, name='index'),
+    path('ranking/', views.ranking, name='ranking'),
+    path('contacto/', views.contacto, name='contacto'),
+    path('author/<int:pk>/',views.AuthorDetailView.as_view(), name='author_detail')
+]
+
+urlpatterns+=[
+    path('pelicula/', views.PeliculaListView.as_view(), name='pelicula_list'),
+    path('pelicula/create/', views.PeliculaCreate.as_view(), name='pelicula_create'),
+    path('pelicula/update/<int:pk>/', views.PeliculaUpdate.as_view(), name='pelicula_update'),
+    path('pelicula/delete/<int:pk>/', views.PeliculaDelete.as_view(), name='pelicula_delete'),
+    path('author/',views.AuthorListView.as_view(), name='author_list'),
+    path('author/create/',views.AuthorCreate.as_view(), name='author_create'),
+    path('author/update/<int:pk>/',views.AuthorUpdate.as_view(), name='author_update'),
+    path('author/delete/<int:pk>/',views.AuthorDelete.as_view(), name='author_delete'),
+]
---
+++ trabajo3/pelicula/views.py
@@ -0,0 +1,63 @@
+from django.shortcuts import render
+from .models import Pelicula,Genero,Author
+from django.views.generic.edit import CreateView,UpdateView,DeleteView
+from django.views.generic import DateDetailView,ListView
+from django.urls import reverse_lazy
+from django.views import generic
+
+# Create your views here.
+
+def index(request):
+    return render(
+        request,
+        'index.html',
+    )
+
+def ranking(request):
+    return render(
+        request,
+        'ranking.html',
+    )
+
+def contacto(request):
+    return render(
+        request,
+        'contacto.html',
+    )
+
+
+class PeliculaCreate(CreateView):
+    model = Pelicula
+    fields = '__all__'
+
+class PeliculaUpdate(UpdateView):
+    model = Pelicula
+    fields = ['titulo','descripcion','autor','genero']
+
+class PeliculaDelete(DeleteView):
+    model = Pelicula
+    success_url = reverse_lazy('index')
+
+class PeliculaDetailView(generic.DetailView):
+    model = Pelicula
+
+class PeliculaListView(ListView):
+    model = Pelicula
+
+class AuthorCreate(CreateView):
+    model = Author
+    fields = '__all__'
+
+class AuthorUpdate(UpdateView):
+    model = Author
+    fields = ['nombre','apellido','fecha_producida']
+
+class AuthorDelete(DeleteView):
+    model = Author
+    success_url = reverse_lazy('index')
+
+class AuthorDetailView(generic.DetailView):
+    model = Author
+
+class AuthorListView(ListView):
+    model = Author
